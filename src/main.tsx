0b3653f8cee37df39f6227f491ee0b5ce967d068
Jeredozaeta <royallotusgiftshop@gmail.com>
2025-06-05 14:01:54 -0500

    Install PostHog and React integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { PostHogProvider } from '@posthog/posthog-js-react';
 import App from './App.tsx';
 import './index.css';
+import { initPostHog } from './utils/posthog';
+
+// Initialize PostHog
+initPostHog();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PostHogProvider>
+      <App />
+    </PostHogProvider>
   </StrictMode>
 );
